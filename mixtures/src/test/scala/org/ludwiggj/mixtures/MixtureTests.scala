1a43b3edaee7da737d34ea46c8b9eafb4ac99835
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2017-10-19 23:47:27 +0100

    Added test cases for three mixtures

--- mixtures/src/test/scala/org/ludwiggj/mixtures/MixtureTests.scala
+++ mixtures/src/test/scala/org/ludwiggj/mixtures/MixtureTests.scala
@@ -4,8 +4,16 @@ import utest._
 
 object MixtureTests extends TestSuite {
   val tests = Tests {
-    "simple potion of two mixtures" - {
+    "potion of two mixtures" - {
       Wizard.mix(Seq(18, 19)) ==> 342
+    }
+
+    "potion of three mixtures (40, 60, 20)" - {
+      Wizard.mix(Seq(40, 60, 20)) ==> 2400
+    }
+
+    "potion of three mixtures (60, 20, 40)" - {
+      Wizard.mix(Seq(60, 20, 40)) ==> 4400
     }
   }
 }
