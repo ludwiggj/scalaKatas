b9399c2c2b013a9f5421d94bc9da2845135ffc74
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2017-10-19 23:43:52 +0100

    Extracted helper method to mix one potion from list of mixtures

--- mixtures/src/test/scala/org/ludwiggj/mixtures/MixtureTests.scala
+++ mixtures/src/test/scala/org/ludwiggj/mixtures/MixtureTests.scala
@@ -5,7 +5,7 @@ import utest._
 object MixtureTests extends TestSuite {
   val tests = Tests {
     "simple potion of two mixtures" - {
-      Wizard.optimumMix(Seq(18, 19)) ==> 342
+      Wizard.mix(Seq(18, 19)) ==> 342
     }
   }
 }
