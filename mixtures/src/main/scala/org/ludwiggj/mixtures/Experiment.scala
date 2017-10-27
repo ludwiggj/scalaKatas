0c90acf1d9fd2858790159f6c9adc8fc3ccb0a04
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2017-10-27 09:16:15 +0100

    Base solution tidied up

--- mixtures/src/main/scala/org/ludwiggj/mixtures/Experiment.scala
+++ mixtures/src/main/scala/org/ludwiggj/mixtures/Experiment.scala
@@ -2,28 +2,40 @@ package org.ludwiggj.mixtures
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
-class Experiment(mixtures: List[Int], smoke: Int = 0, val level: Int = 0) extends Actor with ActorLogging {
-  var results: List[Int] = null
-  var client: ActorRef = null
-  var clientPos: Int = -1
+case class Position(value: Option[Int])
 
-//  log.info(s"Experiment: mixtures [$mixtures], smoke: [$smoke], level: [$level]")
+case class Result(position: Position, smoke: Int) {
+  def positionValue() = position.value.get
+}
+
+case class Mix(position: Position)
+
+class Experiment(mixtures: List[Int], smoke: Int = 0) extends Actor with ActorLogging {
+  var results: List[Int] = _
+  var clientActorRef: ActorRef = _
+  var clientPosition: Position = _
+
+//  log.info(s"Experiment: mixtures [$mixtures], smoke: [$smoke]")
 
   private def mixPair(mixtures: List[Int], i: Int): (Int, Int) = {
     ((mixtures(i) + mixtures(i + 1)) % 100, mixtures(i) * mixtures(i + 1))
   }
 
   override def receive = {
-    case (position: Int) => {
-      client = sender
+    case (mix: Mix) => {
+//      log.info(s"Received [$position]")
+
+      val position = mix.position
+
+      clientActorRef = sender
       mixtures match {
-        case Nil => sender ! (position, 0)
+        case Nil => sender ! Result(position, smoke = 0)
         case mix :: Nil => {
 //                    log.info(s"Single item in list [$mix]")
-          sender ! (position, smoke)
+          sender ! Result(position, smoke)
         }
         case mixtureList => {
-          clientPos = position
+          clientPosition = position
           results = List.fill(mixtures.size - 1)(-1)
 
           (0 to mixtureList.size - 2).toList foreach { i =>
@@ -31,21 +43,20 @@ class Experiment(mixtures: List[Int], smoke: Int = 0, val level: Int = 0) extend
             val (thisMix, thisSmoke) = mixPair(mixtures, i)
             val trailingMixtures = mixtureList drop i + 2
 
-//            log.info(s"Exp: (mix[$mixtures], smoke: [$smoke], lev: [$level]), index [$i] => Exp: (mix[${leadingMixtures ++ List(thisMix) ++ trailingMixtures}], smoke: [${smoke + thisSmoke}], lev: [${level + 1}])")
+//            log.info(s"Exp: (mix[$mixtures], smoke: [$smoke]), index [$i] => Exp: (mix[${leadingMixtures ++ List(thisMix) ++ trailingMixtures}], smoke: [${smoke + thisSmoke}])")
 
             context.actorOf(Props(
-              new Experiment(leadingMixtures ++ List(thisMix) ++ trailingMixtures, smoke = smoke + thisSmoke, level = level + 1)
-            )) ! i
+              new Experiment(leadingMixtures ++ List(thisMix) ++ trailingMixtures, smoke = smoke + thisSmoke)
+            )) ! Mix(Position(Some(i)))
           }
         }
       }
     }
 
-    case (position: Int, result: Int) => {
-      results = results.updated(position, result)
+    case (result: Result) => {
+      results = results.updated(result.positionValue(), result.smoke)
       if (!results.contains(-1)) {
-        //        log.info(s">>>>>>> result: level [$level] result [${results.min}]")
-        client ! (clientPos, results.min)
+        clientActorRef ! Result(clientPosition, results.min)
       }
     }
   }
--- mixtures/src/main/scala/org/ludwiggj/mixtures/Wizard.scala
+++ mixtures/src/main/scala/org/ludwiggj/mixtures/Wizard.scala
@@ -8,21 +8,22 @@ import java.util.concurrent.TimeUnit._
 
 class Wizard extends Actor {
   override def receive = {
-    case (_: Int, result: Int) => {
-      println(s"Position, Result = $result")
+    case (result: Result) => {
+      println(s"Result = ${result.smoke}")
       context.system.terminate()
     }
   }
 }
 
 object Wizard extends App {
+//  val mixtures: List[Int] = List(1, 2, 3)
 //  val mixtures: List[Int] = List(10, 85, 30, 60, 5)
   val mixtures: List[Int] = List(10, 85, 30, 60, 5, 75, 25, 40, 85, 70)
 //  val mixtures: List[Int] = 1 to 22 map (_ * 3) toList
   val system = ActorSystem("wizard")
   val wizard = system.actorOf(Props(new Wizard))
 
-  system.actorOf(Props(new Experiment(mixtures))).tell(-1, wizard)
+  system.actorOf(Props(new Experiment(mixtures))).tell(Mix(Position(None)), wizard)
 
   Await.ready(system.whenTerminated, Duration(1, MINUTES))
 }
